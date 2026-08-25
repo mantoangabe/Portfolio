17b6566a4d8b38bdb8eb0774e456f0107a367177
mantoangabe <35808413+mantoangabe@users.noreply.github.com>
2026-08-25 15:12:18 -0600

    Update ContactPage.jsx

--- portfolio/src/pages/ContactPage.jsx
+++ portfolio/src/pages/ContactPage.jsx
@@ -7,7 +7,7 @@ import {
   Stack,
   Typography,
 } from "@mui/material";
-import bodyshot from "../assets/face.JPG";
+import bodyshot from "../assets/face.jpg";
 
 function ContactPage() {
   return (
